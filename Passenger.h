16a56ab807b02f474e9f2284076603eee8d0b598
vikhan11 <31761451+vikhan11@users.noreply.github.com>
2017-10-26 21:23:02 +0300

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -10,10 +10,15 @@ protected:
 	string surname;
 public:
 	Passenger();
-	void set_name(string _name_);
+	Passenger(const Passenger& ob);
+	Passenger(string _name, string _surname);
+	void set_name(string _name);
 	string get_name() const;
-	void set_surname(string _surname_);
+	void set_surname(string _surname);
 	string get_surname() const;
 	//void info() const;
 	~Passenger();
+
+	friend ostream& operator << (ostream& os, Passenger& passenger);
+	friend istream& operator >> (istream& os, Passenger& passenger);
 };
